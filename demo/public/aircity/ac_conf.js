a3fb7ea2a611e150030d9d50e66d17c9425471d4
sing <lpqst@outlook.com>
2025-04-20 13:39:01 +0800

    1

--- demo/public/aircity/ac_conf.js
+++
@@ -1,7 +0,0 @@
-
-var HostConfig = {
-  "Manager": "127.0.0.1:8080",
-  "Player": "127.0.0.1:8080",
-  "Path": "",
-  "NeedLogin": false
-}
--- demo/src/http/service.ts
+++
@@ -1,7 +0,0 @@
-const Service = {
-    // 请求地址 开发环境下使用代理
-    mdoel1: 'http://127.0.0.1:3300',
-    WeatherId: 'https://geoapi.qweather.com',
-    WeatherData: 'https://devapi.qweather.com',
-};
-export { Service };
